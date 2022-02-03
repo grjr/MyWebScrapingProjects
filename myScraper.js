568e17e2fa0d40b0dba4512966e3de97dd1e9e13
grjr <greety89@gmail.com>
2022-02-03 15:45:49 -0700

    Added sending email functionality

--- myScraper.js
+++ myScraper.js
@@ -1,26 +1,75 @@
+require('dotenv').config()  //this loads all the env variables into process.env
+const sgMail = require('@sendgrid/mail')   //hook up the sendgrid library we imported earlier
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+
 var Nightmare = require('nightmare');      
 var nightmare = Nightmare({
-     show: true,//Display the electron window
+     show: true,  //Display the electron window
      waitTimeout: 1000
 
-});
+})
 
+const args = process.argv.slice(2)
+const url = args[0]
+const minPrice = args[1]
 checkPrice()
 
-
 async function checkPrice(){
-const currPrice = await nightmare
-                 .goto("https://www.amazon.ca/Apple-11-inch-iPad-Pro-Wi-Fi-128GB/dp/B0933LPPKZ")
-                 .wait(".a-price-whole")                                                       //OR .wait("#corePriceDisplay_desktop_feature_div")
-                 .evaluate(() => document.getElementsByClassName("a-price-whole")[0].innerText)//OR .evaluate(()=>document.getElementById("corePriceDisplay_desktop_feature_div").innerText)
-                 .end()
-const currPriceNumber = parseInt(currPrice)  
-//const currPriceNumber = parseFloat(currPrice.replace('$',''))
-if(currPriceNumber < 1000)    
-    console.log("It is cheap") 
-else
-    console.log("It is expensive")   
-    
-console.log("String value: "+currPrice)
-console.log("After converting to number: "+currPriceNumber)
+    try{
+        dvfhvdfhvdvb //add this only to deliberately throw error
+        const currPrice = await nightmare
+                            //.goto("https://www.amazon.ca/Apple-11-inch-iPad-Pro-Wi-Fi-128GB/dp/B0933LPPKZ")
+                            .goto(url)
+                            .wait(".a-price-whole")   //IF USING CLASSNAME
+                            //.wait("#corePriceDisplay_desktop_feature_div") //IF USING ID USE #
+                            .evaluate(() => document.getElementsByClassName("a-price-whole")[0].innerText)
+                            //.evaluate(()=>document.getElementById("corePriceDisplay_desktop_feature_div").innerText)
+                            .end()
+
+    const currPriceNumber = parseInt(currPrice)  
+    //const currPriceNumber = parseFloat(currPrice.replace('$','')) //IF THE ELEMENT CONTAINS $, REPLACE IT
+
+    if(currPriceNumber < minPrice){    //if not dynamically providing minPrice, hardcode a specific number
+        await sendEmail('Price is Low', `The price on ${url} has dropped below ${minPrice}`) 
+        console.log("It is cheap") 
+    }
+    //else  //not needed when sending email, we only need to be notified when the price goes below our minPrice
+    //    console.log("It is expensive")   
+    console.log("Original string value: "+currPrice)
+    console.log("After converting to number: "+currPriceNumber)
+    console.log("Given  minPrice: "+minPrice)
+    }
+
+    catch (e){
+        await sendEmail('Amazon Price Checker Error!!!', e.message)
+        throw e
+    }
 }
+    
+async function sendEmail(subject, body){
+    const email = {
+        to: 'migat58746@chinamkm.com',
+        from: 'greety89@gmail.com',
+        subject:subject,
+        text:body,
+        html:body
+    }
+    return sgMail.send(email)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
